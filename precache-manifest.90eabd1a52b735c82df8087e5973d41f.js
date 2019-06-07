957ee67e77b1f7541e57dbe598c0653683273491
zfletch <zfletch2@gmail.com>
2019-06-07 15:36:19 -0400

    Updates

---
+++ precache-manifest.90eabd1a52b735c82df8087e5973d41f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadb8ac46eac9e5dfdb1f4de1dec3ed0",
+    "url": "/harrington-trees/index.html"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/harrington-trees/static/css/2.ec3f0427.chunk.css"
+  },
+  {
+    "revision": "d0a2a5b49f5f6cdd4487",
+    "url": "/harrington-trees/static/css/main.a730fb68.chunk.css"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/harrington-trees/static/js/2.21d3e77f.chunk.js"
+  },
+  {
+    "revision": "d0a2a5b49f5f6cdd4487",
+    "url": "/harrington-trees/static/js/main.56be4c3e.chunk.js"
+  },
+  {
+    "revision": "d041bc98f1ed6815064b",
+    "url": "/harrington-trees/static/js/runtime~main.058464c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/harrington-trees/precache-manifest.0241498b3fd23275878d15ca1e606577.js"
+  "/harrington-trees/precache-manifest.90eabd1a52b735c82df8087e5973d41f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/harrington-trees/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/harrington-trees/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
